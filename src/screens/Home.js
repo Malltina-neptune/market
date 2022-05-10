b2210c7602a52c45c631f34dfe80bbe415d3cbcd
Maryam <mrymsheikhhadi@gmail.com>
2022-05-10 19:24:42 +0430

    Add home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,41 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
 
 const Home = ({navigation}) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getProducts = async () => {
+     try {
+      const response = await fetch('https://api.malltina.net/search/v2?q=shoes');
+      const json = await response.json();
+      setData(json.products);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <View>
-      <Text
-        onPress={() => {
-          navigation.navigate('result');
-        }}>
-        Home
-      </Text>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}</Text>
+          )}
+        />
+      )}
     </View>
   );
 };
 
 export default Home;
+
