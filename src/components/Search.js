21ac1636c1e9033e59352b1900f3a06c25b7aac7
saghari97 <saghargorjiduz97@gmail.com>
2022-05-11 18:48:52 +0430

    added close icon to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, TextInput, Image, StyleSheet} from 'react-native';
+import {View, TextInput, Image, StyleSheet , TouchableOpacity} from 'react-native';
 
 function Search({searchValue, setSearchValue, onSubmit, onClear}) {
   return (
