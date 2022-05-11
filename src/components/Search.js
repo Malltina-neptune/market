b9175b93e921c17e27fbe9be5f776295a4b7e6b9
Mohammad Reza Salemi <en.salemi75@gmail.com>
2022-05-11 20:00:04 +0430

    Fix Search Style

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {View, TextInput, Image, StyleSheet , TouchableOpacity} from 'react-native';
+import {
+  View,
+  TextInput,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 function Search({searchValue, setSearchValue, onSubmit, onClear}) {
   return (
-    <View style={styles.searchBox}>
-      <View style={styles.mainView}>
-        <Image
-          style={styles.imgStyle}
-          source={require('../assets/search.png')}
-        />
-        <TextInput
-          value={searchValue}
-          onChangeText={setSearchValue}
-          onSubmitEditing={onSubmit}
-        />
-      </View>
-      <TouchableOpacity style={styles.closeButtonParent} onPress={onClear}>
+    <View style={styles.mainView}>
+      <Image style={styles.imgStyle} source={require('../assets/search.png')} />
+      <TextInput
+        style={styles.input}
+        value={searchValue}
+        onChangeText={setSearchValue}
+        onSubmitEditing={onSubmit}
+      />
+      <TouchableOpacity onPress={onClear}>
         <Image
           style={styles.closeButton}
           source={require('../assets/close.png')}
@@ -24,26 +26,24 @@ function Search({searchValue, setSearchValue, onSubmit, onClear}) {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   input: {
-    backgroundColor: 'lightgray',
-    borderRadius: 30,
-    overflow: 'hidden',
-    flexDirection: 'row',
-    alignItems: 'center',
+    flex: 1,
   },
   imgStyle: {
     height: 20,
     width: 20,
-    marginRight: 15,
+    marginRight: 4,
   },
   mainView: {
+    width: '100%',
+    backgroundColor: 'lightgray',
     flexDirection: 'row',
-    padding: 10,
-  },
-  closeButtonParent: {
-    position: 'absolute',
-    right: 20,
+    borderRadius: 25,
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    alignItems: 'center',
   },
 });
 export default Search;
