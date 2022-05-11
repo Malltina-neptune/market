66e011a0e26c7c75fd9279598c7f3420a129450a
Alireza Razinejad <a.razinejad@gmail.com>
2022-05-11 09:23:42 +0430

    IT-27: Lint the component and destructed the props

--- src/components/ProductPrice.js
+++ src/components/ProductPrice.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-const ProductPrice = (mainPrice, discount) => {
+
+const ProductPrice = ({mainPrice, discount}) => {
   return (
     <>
       <View>
